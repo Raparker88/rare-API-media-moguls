bbcc0bccc1efbe47e2eab40021767f8137b3e10d
Connor Blakeney <connorblakeney@yahoo.com>
2020-11-10 11:58:39 -0600

    fixed rareuser model, will fix again with conflicts

--- rareapi/models/rareuser.py
+++ rareapi/models/rareuser.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class RareUser(models.Model):
     """RareUser Model"""
     bio = models.CharField(max_length=500)
-    profile_img_url = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    profile_image_url = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_on = models.DateField()
