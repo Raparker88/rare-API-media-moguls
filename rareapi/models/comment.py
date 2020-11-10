8b0e00cf86dadf73a604763db1ff52020b49e094
michaelc321 <michaelc321@yahoo.com>
2020-11-10 10:32:03 -0600

    migrate

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -7,5 +7,5 @@ class Comment(models.Model):
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
     subject = models.CharField(max_length=500)
-    created_on = models.DateTimeField(auto_now=True, auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     
