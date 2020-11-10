d9e138568d125ccb9f17ecaec02197d4b60e1351
Connor Blakeney <connorblakeney@yahoo.com>
2020-11-10 14:45:30 -0600

    changed post viewset to posts

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rareapi.models import Post, RareUser
 
-class Post(ViewSet):
+class Posts(ViewSet):
     def list(self, request):
 
         posts = Post.objects.all()
