d7acdee19231b56a808f1a335b872466c02e296b
Raparker88 <raparker88@gmail.com>
2020-11-10 13:57:07 -0600

    created posttag.py view file with list method to get posttags by post

---
+++ rareapi/views/posttag.py
@@ -0,0 +1,27 @@
+"""View module for handling requests about posttags"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rareapi.models import PostTag, Tag, Post
+
+class PostTags(ViewSet):
+    """Rare post tags"""
+
+    def list(self, request):
+        """Handle GET requests to get posttags by post"""
+
+        posttags = PostTag.objects.all()
+
+        #filtering posttags by post
+        post = self.request.query_params.get("postId", None)
+
+        if post is not None:
+            posttags = posttags.filter(post_id=post)
+
+        serializer = PostTagSerializer(
+            posttags, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class PostTagSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for posttags"""
