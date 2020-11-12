e6b30fc959d7ee892eff3a59530b1b8667991868
Raparker88 <raparker88@gmail.com>
2020-11-12 17:41:22 -0600

    added action in user view to get current user, took id off rareuser attached to posts

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -134,7 +134,7 @@ class Posts(ViewSet):
 class PostRareUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = RareUser
-        fields = ('id', 'username', 'is_active', 'is_staff', 'email', 'full_name')
+        fields = ('username', 'is_active', 'is_staff', 'email', 'full_name')
 
 """Basic Serializer for single post"""
 class PostSerializer(serializers.ModelSerializer):
--- rareapi/views/user.py
+++ rareapi/views/user.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
+from rest_framework.decorators import action
 from django.contrib.auth.models import User
 
 class Users(ViewSet):
@@ -27,10 +28,10 @@ class Users(ViewSet):
     @action(methods=['get'], detail=False)
     def current_user(self, request):
         current_user = request.auth.user
-	
-	    serializer = UserSerializer(
-            current_user, context={'request': request})
-	    return Response(serializer.data)
+        
+        serializer = UserSerializer(current_user, context={'request': request})
+        
+        return Response(serializer.data)
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for rareusers
