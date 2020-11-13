d9f1e76e4dbe095b06e234c0c2a0f3091b201e87
Connor Blakeney <connorblakeney@yahoo.com>
2020-11-13 09:37:12 -0600

    reverted changes

--- rareapi/views/user.py
+++ rareapi/views/user.py
@@ -5,24 +5,18 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from rareapi.models import RareUser, Post
 from rest_framework.decorators import action
 
 class Users(ViewSet):
     """Users"""
-
-
     def list(self, request):
         """Handle GET requests to users resource
-
         Returns:
             Response -- JSON serialized list of users
         """
         users = User.objects.all()
-
-
         serializer = UserSerializer(
             users, many=True, context={'request': request})
         return Response(serializer.data)
@@ -33,21 +27,16 @@ class Users(ViewSet):
         posts = Post.objects.filter(rareuser=rareuser)
 
         serializer = PostSerializer(posts, many=True, context={'request':request})
-    def current_user(self, request):
-        current_user = request.auth.user
-        
-        serializer = UserSerializer(current_user, context={'request': request})
-        
         return Response(serializer.data)
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for rareusers
-
     Arguments:
         serializers
     """
     class Meta:
         model = User
+        fields = ('id', 'username', 'is_staff', 'is_active') 
         fields = ('id', 'username', 'is_staff', 'is_active')
 
 """Serializer for RareUser Info in a post"""         
@@ -63,4 +52,3 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('id', 'title', 'publication_date', 'content', 'rareuser', 'category', 'approved')
         depth = 1
-        fields = ('username', 'is_staff', 'is_active', 'email', 'date_joined', 'first_name', 'last_name')
