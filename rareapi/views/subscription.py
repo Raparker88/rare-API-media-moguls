25d74ca21ee8153d29b40b25c074a82e46cab02b
Austin Phy <atphy42@gmail.com>
2020-11-16 14:48:29 -0600

    added GET method for subscriptions by logged in user

---
+++ rareapi/views/subscription.py
@@ -0,0 +1,46 @@
+"""View module for handling requests about posttags"""
+from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from rareapi.models import Subscription, RareUser
+
+class Subscriptions(ViewSet):
+    """Rare subscriptions"""
+
+    def list(self, request):
+        """Handle GET requests to get subscriptions by user"""
+
+        subscriptions = Subscription.objects.all()
+
+        #filtering subscriptions by user
+        follower = RareUser.objects.get(user=request.auth.user)
+
+        if follower is not None:
+            subscriptions = subscriptions.filter(follower_id=follower)
+
+        serializer = SubscriptionSerializer(
+            subscriptions, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class RareUserSerializer(serializers.ModelSerializer):
+    """JSON serializer for post author's related Django user"""
+    class Meta:
+        model = RareUser
+        fields = ['id', 'username']
+
+class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for comments"""
+    follower_id = RareUserSerializer(many=False)
+    author_id = RareUserSerializer(many=False)
+
+    class Meta:
+        model = Subscription
+        url = serializers.HyperlinkedIdentityField(
+            view_name='subscription',
+            lookup_field='id'
+        )
+        fields = ('id', 'follower_id', 'author_id', 'created_on', 'ended_on')
