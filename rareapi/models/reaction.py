8ff3824365ae1590d5a3a5282ad0ff70979f2cf1
Raparker88 <raparker88@gmail.com>
2020-11-17 09:56:01 -0600

    created unmapped property of count on reactions and a custom action react that adds rows to the postreaction table

--- rareapi/models/reaction.py
+++ rareapi/models/reaction.py
@@ -5,3 +5,11 @@ class Reaction(models.Model):
     """Reaction Model"""
     label = models.CharField(max_length=25)
     image_url = models.ImageField(upload_to="emojis/", null = False)
+
+    @property
+    def count(self):
+        return self.__count
+
+    @count.setter
+    def count(self, value):
+        self.__count = value
--- rareapi/views/reaction.py
+++ rareapi/views/reaction.py
@@ -5,7 +5,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from rareapi.models import Reaction
+from rest_framework.decorators import action
+from rareapi.models import Reaction, PostReaction, Post, RareUser
 
 class Reactions(ViewSet):
     """rare reactions"""
@@ -18,10 +19,44 @@ class Reactions(ViewSet):
         """
         reactions = Reaction.objects.all()
 
+        post_id = self.request.query_params.get('post_id', None)
+        if post_id is not None:
+            for reaction in reactions:
+                post_reactions = PostReaction.objects.filter(post_id=post_id)
+                final_count = post_reactions.filter(reaction=reaction)
+                reaction.count = len(final_count)
+
 
         serializer = ReactionSerializer(
             reactions, many=True, context={'request': request})
         return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
+    def react(self, request, pk=None):
+        """Managing users reacting to posts"""
+
+        if request.method == "POST":
+            post = Post.objects.get(pk=request.data["post_id"])
+            rareuser = RareUser.objects.get(user=request.auth.user)
+            reaction = Reaction.objects.get(pk=pk)
+
+            try:
+                # Determine if the user already reacted
+                like = PostReaction.objects.get(
+                    post=post, user=rareuser, reaction=reaction)
+                return Response(
+                    {'message': 'User already clicked this reaction.'},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            except PostReaction.DoesNotExist:
+                # The user is not signed up.
+                like = PostReaction()
+                like.post = post
+                like.user = rareuser
+                like.reaction = reaction
+                like.save()
+
+                return Response({}, status=status.HTTP_201_CREATED)
 
    
 
@@ -33,4 +68,4 @@ class ReactionSerializer(serializers.HyperlinkedModelSerializer):
     """
     class Meta:
         model = Reaction
-        fields = ('id', 'label', "image_url")
+        fields = ('id', 'label', "image_url", 'count')
